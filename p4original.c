f8375d6c8a7735222bb77b74dc7ff4e56ac0e226
chatur 16 <94945136+chatur76@users.noreply.github.com>
2021-12-03 03:14:52 +0000

    fhy

--- p4original.c
+++ p4original.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
-void input(int a,int b,intc);
-void cmp(int a,int b,int c);
-int output(int a,int b,intc);
+int input();
+int largest(int a,int b,int c);
+void output(int larg);
 int main()
 {
-int a,b,c
+  int a,b,c,larg;
+  a=input();
+  b=input();
+  c=input();
+  larg=largest(a,b,c);
+  output(larg);
+  return 0;
+}
+int largest(int a,int b,int c)
+{
+  if(a>b && a>c)
+  return a;
+  if(b>a && b>c)
+  return b;
+  else
+  return c;
+}
+void output(int larg)
+{
+  printf("the largest number is %d",larg);
+}
+int input()
+{
+  int a;
+  printf("enter a number");
+  scanf("%d",&a);
+  return a;
 }
